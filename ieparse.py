1906f89d811998dd5a806425f01b943b9b324c3a
Jaroslav Benkovsky <edheldil@users.sourceforge.net>
2009-06-05 21:18:06 +0000

    Added -o option, allows setting options, e.g. stream.debug_coverage

--- ieparse.py
+++ ieparse.py
@@ -15,10 +15,18 @@ if len (sys.argv) > 1 and sys.argv[1] == '-v':
     core.set_option ('format.tis.print_palettes', True)
     sys.argv.pop (1)
 
+if len (sys.argv) > 1 and sys.argv[1] == '-o':
+    sys.argv.pop (1)
+    option = sys.argv.pop (1)
+    core.set_option (option, True)
+
+
 if len (sys.argv) > 1:
     ffile = sys.argv[1]
 else:
-    raise ValueError ("missing arg!")
+    print """Usage:
+    %s  [-v] [-o option ] <file>""" %sys.argv[0]
+    sys.exit (1)
 
 
 src = stream.FileStream ().open (ffile)
