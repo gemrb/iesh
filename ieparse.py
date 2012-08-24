e53421f625b37eea4ca9939a09c114d9bb17d044
Jaroslav Benkovsky <edheldil@users.sourceforge.net>
2012-08-24 18:49:01 +0200

    Parse and use option option's value if any

--- ieparse.py
+++ ieparse.py
@@ -18,14 +18,30 @@ if len (sys.argv) > 1 and sys.argv[1] == '-v':
 if len (sys.argv) > 1 and sys.argv[1] == '-o':
     sys.argv.pop (1)
     option = sys.argv.pop (1)
-    core.set_option (option, True)
+    try:
+        option, value = option.split ('=')
+    except ValueError:
+        value = True
+
+    # Convert non-string values
+    if value == 'None':
+        value = None
+    elif value == 'True':
+        value = True
+    elif value == 'False':
+        value = False
+    else:
+        try: value = int(value)
+        except ValueError: pass
+
+    core.set_option (option, value)
 
 
 if len (sys.argv) > 1:
     ffile = sys.argv[1]
 else:
     print """Usage:
-    %s  [-v] [-o option ] <file>""" %sys.argv[0]
+    %s  [-v] [-o option[=value] ] <file>""" %sys.argv[0]
     sys.exit (1)
 
 
