61047b1353d4487dedb464e47599736eb1eae25d
Jaroslav Benkovsky <edheldil@users.sourceforge.net>
2012-12-21 17:35:10 +0100

    Parse multiple -o options in ieparse

--- ieparse.py
+++ ieparse.py
@@ -5,36 +5,40 @@ import sys
 from infinity import core, stream
 from infinity.formats import *
 
-if len (sys.argv) > 1 and sys.argv[1] == '-v':
-    core.set_option ('format.print_offset', True) 
-    core.set_option ('format.print_size', True) 
-    core.set_option ('format.print_type', True) 
-    core.set_option ('format.mos.print_tiles', True) 
-    core.set_option ('format.mos.print_palettes', True)
-    core.set_option ('format.tis.print_tiles', True)
-    core.set_option ('format.tis.print_palettes', True)
-    sys.argv.pop (1)
+while len (sys.argv) > 1:
+    if sys.argv[1] == '-v':
+        core.set_option ('format.print_offset', True) 
+        core.set_option ('format.print_size', True) 
+        core.set_option ('format.print_type', True) 
+        core.set_option ('format.mos.print_tiles', True) 
+        core.set_option ('format.mos.print_palettes', True)
+        core.set_option ('format.tis.print_tiles', True)
+        core.set_option ('format.tis.print_palettes', True)
+        sys.argv.pop (1)
 
-if len (sys.argv) > 1 and sys.argv[1] == '-o':
-    sys.argv.pop (1)
-    option = sys.argv.pop (1)
-    try:
-        option, value = option.split ('=')
-    except ValueError:
-        value = True
+    elif sys.argv[1] == '-o':
+        sys.argv.pop (1)
+        option = sys.argv.pop (1)
+        try:
+            option, value = option.split ('=')
+        except ValueError:
+            value = True
 
-    # Convert non-string values
-    if value == 'None':
-        value = None
-    elif value == 'True':
-        value = True
-    elif value == 'False':
-        value = False
+        # Convert non-string values
+        if value == 'None':
+            value = None
+        elif value == 'True':
+            value = True
+        elif value == 'False':
+            value = False
+        else:
+            try: value = int(value)
+            except ValueError: pass
+
+        core.set_option (option, value)
+
     else:
-        try: value = int(value)
-        except ValueError: pass
-
-    core.set_option (option, value)
+        break
 
 
 if len (sys.argv) > 1:
