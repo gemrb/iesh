832645a4bc49b531d4fe17667df33289930872b8
Jaka Kranjc <lynxlupodian@users.sourceforge.net>
2014-08-03 21:54:03 +0200

    exmaples.py: replaced the obsolete iterate_objects_by_type example

--- examples.py
+++ examples.py
@@ -1,1 +1,12 @@
-iterate_objects_by_type(0x03f1, lambda obj: printf("%04x", loaded_object(obj).header['animation_id']))
+# finding the maximum value of a stat
+# remember to load game data first!
+# look at restype_list in core.py to get format type values
+cretype = 0x3f1
+max = -100
+name = ""
+for r, o in ObjectIterator(type=cretype, names="none"):
+     t = load_object (r['resref_name'], cretype).header['morale_recovery_time']
+     if t > max:
+         max = t
+         name = r['resref_name']
+print max, " first found in ", name
