26c1d6e6a60f61addcb27fb6c57974d307d5415d
Jaroslav Benkovsky <edheldil@users.sourceforge.net>
2015-05-20 01:08:02 +0200

    Fix several missing self's in dlg_debugger
      * also some whitespace cleanups
      * start to add mock engine

--- infinity/dlg_debugger.py
+++ infinity/dlg_debugger.py
@@ -10,6 +10,17 @@ import infinity.builtins
 import infinity.formats.dlg
 from infinity import core
 
+class MockEngine(object):
+    def __init__(self):
+        self.reset()
+
+    def reset (self):
+        self.dialogs = {}
+        self.values = {}
+        self.actions = {}
+        self.items = {}
+        self.actors = {}
+
 
 class DLGDebugger (object):
     data_dir = '.'
@@ -64,7 +75,7 @@ class DLGDebugger (object):
             resref = name
 
         #print name, resref
-        
+
         if self.dialogs.has_key (resref):
             return self.dialogs[resref]
         else:
@@ -79,12 +90,11 @@ class DLGDebugger (object):
             if a == '':
                 return None
             if len (a) >= 2 and a[0] == '"' and a[-1] == '"':
-                return a[1:-1]            
+                return a[1:-1]
             try:
                 return int (a)
             except:
                 return a
-
 
         #print 'parse:',  cond
         g = DLGDebugger.global_re.findall (cond)
@@ -99,7 +109,7 @@ class DLGDebugger (object):
             except KeyError:
                 self.values[key] = default
                 return default
-            
+
         cond = cond.replace ("\r",  "")
         cond = cond.replace ("\n",  "")
 
@@ -115,7 +125,7 @@ class DLGDebugger (object):
             if not self.values.has_key (cond):
                 self.values[cond] = False
             return
-           
+
         for c in parsed:
             expr = c[0]
             neg = c[1]
@@ -136,15 +146,15 @@ class DLGDebugger (object):
                         value = args[2] > value
                     elif fn == 'globalgt':
                         value = args[2] < value
-        
+
             elif fn == 'globalorglobal':
-                key = ['global:' + args[1].upper () + ':' + args[0], 
+                key = ['global:' + args[1].upper () + ':' + args[0],
                             'global:' + args[3].upper () + ':' + args[2]]
                 if is_merge:
                     value = [0, 0]
                 else:
                     value = self.values[key[0]] or self.values[key[1]]
-                    
+
             elif fn in ('moralelt',  'moralegt'):
                 key = 'PC:morale:' + args[0].upper ()
                 if is_merge:
@@ -155,7 +165,7 @@ class DLGDebugger (object):
                         value = args[2] > value
                     elif fn == 'moralegt':
                         value = args[2] < value
-                    
+
             elif fn in ('partygold', 'partygoldlt',  'partygoldgt'):
                 key = 'Party:gold'
                 if is_merge:
@@ -168,7 +178,7 @@ class DLGDebugger (object):
                         value = args[0] < value
                     else:
                         value = args[0] == value
-                    
+
             elif fn in ('checkstat', 'checkstatlt',  'checkstatgt'):
                 key = 'PC:' + args[2].upper () + ':' + args[0].upper ()
                 if is_merge:
@@ -181,7 +191,7 @@ class DLGDebugger (object):
                         value = args[1] < value
                     else:
                         value = args[1] == value
-        
+
             elif fn in ('proficiency', 'proficiencylt',  'proficiencygt'):
                 key = 'PC:proficiency:' + args[0].upper () + ':' + args[1].upper ()
                 if is_merge:
@@ -194,7 +204,7 @@ class DLGDebugger (object):
                         value = args[2] < value
                     else:
                         value = args[2] == value
-        
+
             elif fn in ('hplt', 'hpgt',  'hppercentlt',  'hppercentgt',  'hppercent'):
                 key = ( 'PC:' + args[0].upper () + ':hp',  'PC:' + args[0].upper () + ':hpmax' )
                 if is_merge:
@@ -205,9 +215,9 @@ class DLGDebugger (object):
                         value2 = 100
                     else:
                         value2 = value / self.values[key[1]]
-            
+
                     if fn == 'hplt':
-                        value = value < args[1] 
+                        value = value < args[1]
                     elif fn == 'hpgt':
                         value = value > args[1]
                     elif fn == 'hppercentlt':
@@ -216,7 +226,7 @@ class DLGDebugger (object):
                         value = value2 > args[1]
                     elif fn == 'hppercent':
                         value = value2 == args[1]
-                
+
             elif fn == 'class':
                 key = 'PC:class:' + args[0].upper ()
                 if is_merge:
@@ -224,21 +234,21 @@ class DLGDebugger (object):
                 else:
                     value = self.values[key]
                     value = value == args[1].upper ()
-        
+
             elif fn == 'nearbydialog':
                 key = 'nearbydialog:' + args[0].upper ()
                 if is_merge:
                     value = False
                 else:
                     value = self.values[key]
-        
+
             elif fn == 'dead':
                 key = 'dead:' + args[0].upper ()
                 if is_merge:
                     value = False
                 else:
                     value = self.values[key]
-        
+
             elif fn == 'inparty':
                 key = 'Party'
                 if is_merge:
@@ -262,7 +272,7 @@ class DLGDebugger (object):
 
             elif fn == 'hasitem':
                 key = 'actor:' + args[1].upper () + ':inv'
-                items[args[0]] = 1
+                self.items[args[0]] = 1
 
                 if is_merge:
                     value = {}
@@ -273,10 +283,10 @@ class DLGDebugger (object):
                     except:
                         value = 0
                     value = value > 0
-                    
+
             elif fn == 'partyhasitem':
-                items[args[0]] = 1
-                
+                self.items[args[0]] = 1
+
                 if is_merge:
                     key = []
                     value = []
@@ -288,10 +298,10 @@ class DLGDebugger (object):
                         if inv.has_key (args[0]):
                             value = True
                             break
-                            
+
             elif fn in ('partyitemcountlt', 'partyitemcountgt'):
-                items[args[0]] = 1
-                
+                self.items[args[0]] = 1
+
                 if is_merge:
                     key = []
                     value = []
@@ -302,20 +312,20 @@ class DLGDebugger (object):
                         inv = get_value (key,  {})
                         if inv.has_key (args[0]):
                             value += inv[args[0]]
-                            
+
                     if fn.endswith ('lt'):
                         value = value < args[1]
                     elif fn.endswith ('gt'):
                         value = value > args[1]
-                    
+
             elif fn == 'true':
                 key = 'true'
                 value = True
-        
+
             elif fn == 'false':
                 key = 'false'
                 value = False
-        
+
             else:
                 #print 'unrecognized cond:',  expr
                 key = expr
@@ -325,7 +335,7 @@ class DLGDebugger (object):
                     value = False
                 else:
                     value = self.values[key]
-        
+
             if is_merge:
                 if type (key) == type (''):
                     key = [ key ]
@@ -338,7 +348,7 @@ class DLGDebugger (object):
                         value = not value
                     if not value:
                         return False
-        
+
         if not is_merge:
             return True
 
@@ -352,55 +362,53 @@ class DLGDebugger (object):
             self.actions[action['code']] = 1
 
     def save_debugger_state (self):
-        s = [ dialogs, values, actions ]
+        s = [ self.dialogs, self.values, self.actions ]
         fh = open ("debugger_state.dat",  "w")
         cPickle.dump (s,  fh)
         fh.close ()
 
     def restore_debugger_state (self):
-        global dialogs, values, actions,  items
-        
         fh = open ("debugger_state.dat",  "r")
         s = cPickle.load (fh)
         fh.close ()
-        dialogs, values, actions  = s
+        self.dialogs, self.values, self.actions = s
 
 
     def merge_all (self):
         def merge_one (d):
             try:
-                merge_conditions (d)
+                self.merge_conditions (d)
             except:
                 #print "!!! Merge exception in %s !!!" %d.name
                 print("!!! Merge exception !!!")
-        
+
         infinity.builtins.iterate_objects_by_type (0x3f3,  merge_one)
 
     def print_conditions (self, cond = None):
         if cond is None:
-            keys = values.keys ()
+            keys = self.values.keys ()
             keys.sort ()
-        elif values.has_key (cond):
+        elif self.values.has_key (cond):
             keys = [ cond ]
         else:
             keys = []
-            
+
         for key in keys:
-            print(key,  '=',  repr (values[key]))
+            print(key, '=', repr (self.values[key]))
 
 
     def set_condition_value (self, cond, value = None):
         try:
-            oldvalue = values[cond]
+            oldvalue = self.values[cond]
         except KeyError:
             # FIXME: maybe try to parse the condition first and test the parsed var?
             print("Condition does not exist:",  cond)
             return
-        
+
         if value == None:
             value = not oldvalue
 
-        values[cond] = value
+        self.values[cond] = value
 
 
 
@@ -412,12 +420,12 @@ class DLGDebugger (object):
 
         j = 0
         for transition in d.transition_list[state['first_transition_index']:state['first_transition_index']+state['transition_cnt']]:
-            if (transition['flags'] & 0x02) and transition['trigger_index'] != 0xffffffff and not eval_trigger (d.transition_trigger_list[transition['trigger_index']]):
+            if (transition['flags'] & 0x02) and transition['trigger_index'] != 0xffffffff and not self.eval_trigger (d.transition_trigger_list[transition['trigger_index']]):
                 j += 1
                 continue
 
             print('[%d]' %j, end=' ')
-            print_transition (transition)
+            self.print_transition (transition)
             j += 1
 
 
@@ -442,13 +450,13 @@ class DLGDebugger (object):
 
 
     def eval_trigger (self, trigger):
-        return  eval_function (trigger['code'])
+        return  self.eval_function (trigger['code'])
 
 
     def get_next_state (self, d, state_ndx = 0):
         while True:
             state = d.state_list[state_ndx]
-            if state['trigger_index'] == 0xffffffff or eval_trigger (d.state_trigger_list[state['trigger_index']]):
+            if state['trigger_index'] == 0xffffffff or self.eval_trigger (d.state_trigger_list[state['trigger_index']]):
                 return state_ndx
             else:
                 state_ndx += 1
@@ -457,7 +465,7 @@ class DLGDebugger (object):
 
     def do_transition (self, d, state, transition_ndx):
         transition = d.transition_list[state['first_transition_index'] + transition_ndx]
-        
+
         if (transition['flags'] & 0x01) and transition['pc_text'] != 0xffffffff:
             if core.strrefs:
                 print('SELF:', core.strrefs.strref_list[transition['pc_text']]['string'])
@@ -482,7 +490,7 @@ class DLGDebugger (object):
             args = None
 
         cmd = cmd.lower ()
-        
+
         if cmd == '' and args is None:
             return cmd, args
 
@@ -494,11 +502,11 @@ class DLGDebugger (object):
         for c in DLGDebugger.commands:
             if c[0].startswith (cmd):
                 fcmds.append (c[0])
-        
+
         if len (fcmds) == 0:
             print ('Unknown command:', cmd)
             return None, None
-        
+
         elif len (fcmds) > 1:
             print ("Command '%s' may be:", ', '.join (fcmds))
             return None, None
@@ -510,7 +518,6 @@ class DLGDebugger (object):
 
 
     def run_dialog (self, name, state = 0):
-        global values,  dialogs
         #values = {}
         #dialogs = {}
 
@@ -530,7 +537,7 @@ class DLGDebugger (object):
             #debugger_shell (name,  state)
             if next_dlg is None:
                 d = None
-            elif type (next_dlg) == type (""):  
+            elif type (next_dlg) == type (""):
                 d = self.get_dialog (name)
             else:
                 d = next_dlg
@@ -558,15 +565,15 @@ class DLGDebugger (object):
                         print("help on " + args)
                 elif cmd == 'env':
                     if args is None:
-                        print_conditions ()
+                        self.print_conditions ()
                     else:
                         try:
                             (cond,  value) = re.split('\s*=\s*',  args,  2)
                         except ValueError:
                             cond,  value = args,  None
-                        
+
                         if value is None:
-                            print_conditions (cond)
+                            self.print_conditions (cond)
                         else:
                             if value in ('False',  'True'):
                                 value = value == 'True'
@@ -575,8 +582,8 @@ class DLGDebugger (object):
                                     value = int (value)
                                 except:
                                     pass
-        
-                            set_condition_value (cond,  value)
+
+                            self.set_condition_value (cond, value)
 
                 elif cmd == 'ps':
                     if args is None:
@@ -587,12 +594,12 @@ class DLGDebugger (object):
                     print("State #%d:" %s)
                     state_obj = d.state_list[s]
                     d.print_struc (state_obj, d.state_desc)
-                    
+
                     if state_obj['trigger_index'] != 0xffffffff:
                         print("State trigger:")
                         script_obj = d.state_trigger_list[state_obj['trigger_index']]
                         d.print_script (script_obj)
-                        
+
                         try:
                             print('Evals to:', self.eval_function (script_obj['code']))
                         except KeyError:
@@ -604,24 +611,24 @@ class DLGDebugger (object):
                         d.print_transition (transition)
                         print("\n")
                         j = j + 1
-                    
+
                 elif cmd == 'run':
                     # FIXME: optional arguments!
                     # FIXME: return to the last run dlg instead of the first one?
                     print("dlg reset")
                     step = False
-                    values = {}
-                    dialogs = {}
-                    d = get_dialog (name)
+                    self.values = {}
+                    self.dialogs = {}
+                    d = self.get_dialog (name)
                     state = 0
                     address = DLGDebugger.A_GET_STATE
-                    
+
                 elif cmd == 'trigger':
                     try:
-                        print(eval_function (args))
+                        print(self.eval_function (args))
                     except KeyError:
                         print("Unknown trigger:",  args)
-                        
+
                 elif cmd == 'continue':
                     step = False
                     break
@@ -639,17 +646,17 @@ class DLGDebugger (object):
                 # TODO: execution history with 'undo' possibility
 
             if address == DLGDebugger.A_GET_STATE:
-                state = get_next_state (d,  state)
+                state = self.get_next_state (d, state)
                 address = DLGDebugger.A_GET_REPLY
-            
+
             if address == DLGDebugger.A_GET_REPLY:
-                print_state (d,  d.state_list[state])
-                
+                self.print_state (d, d.state_list[state])
+
             if address == DLGDebugger.A_TRANSITION:
-                next_dlg,  state = do_transition (d,  d.state_list[state],  int (cmd))
+                next_dlg, state = self.do_transition (d, d.state_list[state], int (cmd))
                 address = DLGDebugger.A_GET_STATE
-            
-    
+
+
 if __name__ == '__main__':
     infinity.builtins.load_game ("/home/benkovsk/dos/pst")
     dd = DLGDebugger()
